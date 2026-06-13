4e27997281efdfb023d831283cd45bbc5b8d7c93
David <akinwoleawobodudavid1@gmail.com>
2026-06-14 00:19:30 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,25 @@ import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Requestform from "./pages/Requestform";
 import Confirmation from "./pages/Confirmation";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-      path="/confirmation" element={<Confirmation />}
-      />
-      <Route
-      path="/dashboard" element={<Dashboard />}
-      />
-      <Route
-      path="/request" element={<Requestform />}
-      />    
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/confirmation" element={<Confirmation />}
+        />
+        <Route
+          path="/dashboard" element={<Dashboard />}
+        />
+        <Route
+         path="/request" element={<Requestform />}
+        />    
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+import { FaLifeRing } from "react-icons/fa";
+
+function Navbar () {
+    return (
+        <nav>
+            <div className="logo">
+                <div className="logo-icon">
+                    <FaLifeRing />
+                </div>
+                <h2>IT Service Portal</h2>
+            </div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/request">Submit Request</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
